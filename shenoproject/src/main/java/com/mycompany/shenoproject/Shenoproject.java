043707c22cea8a5be16bd7a5d9f4a80f69803b75
Sheno2207 <sheno.502252@sxcce.edu.in>
2025-01-30 14:34:45 +0530

    Add files via upload

---
+++ shenoproject/src/main/java/com/mycompany/shenoproject/Shenoproject.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.shenoproject;
+import java.util.*;
+
+/**
+ *
+ * @author IT20
+ */
+public class Shenoproject {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the first number:");
+        int a=sc.nextInt();
+     
+        
+        System.out.println("Enter the second number:");
+         int b=sc.nextInt();
+         int c=a+b;
+        System.out.println("The addition of 2 numbers is" + c);
+        
+    }
+}
